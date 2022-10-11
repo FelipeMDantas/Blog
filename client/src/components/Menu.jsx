274934393abb360fe70541c167876a46578c8e94
FelipeMDantas <felipemdantas@terra.com.br>
2022-10-10 23:09:31 -0300

    Write page stylized and backend structure initialized.

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
 const Menu = () => {
-  return <div>Menu</div>;
+  const posts = [
+    {
+      id: 1,
+      title: "Some title",
+      desc: "Some description",
+      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "Some title",
+      desc: "Some description",
+      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "Some title",
+      desc: "Some description",
+      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      title: "Some title",
+      desc: "Some description",
+      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
+  return (
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
