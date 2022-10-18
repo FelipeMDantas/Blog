3e697afdcfbc38e6271f35846e32c8c6a6487aea
FelipeMDantas <felipemdantas@terra.com.br>
2022-10-17 23:05:44 -0300

    Relation between posts and image upload function added.

--- api/index.js
+++ api/index.js
@@ -3,11 +3,19 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const upload = multer({ dest: "./uploads/" });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  res.status(200).json("Image has been uploaded.");
+});
+
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,30 +1,47 @@
-const Menu = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "Some title",
-      desc: "Some description",
-      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      title: "Some title",
-      desc: "Some description",
-      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      title: "Some title",
-      desc: "Some description",
-      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 4,
-      title: "Some title",
-      desc: "Some description",
-      img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-  ];
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Menu = ({ cat }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?cat=${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "Some title",
+  //     desc: "Some description",
+  //     img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Some title",
+  //     desc: "Some description",
+  //     img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Some title",
+  //     desc: "Some description",
+  //     img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Some title",
+  //     desc: "Some description",
+  //     img: "https://images.pexels.com/photos/13437426/pexels-photo-13437426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  // ];
 
   return (
     <div className="menu">
